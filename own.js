330814e7b3d9c1ce8a285725b7747c28cb817ce9
CreamyMilk <62656400+CreamyMilk@users.noreply.github.com>
2024-03-21 14:29:34 +0300

    feat : make more configurable

--- own.js
+++ own.js
@@ -1,14 +1,30 @@
 let baseURL = "https://auth.gandalf.network"
 
-let config = {
+
+function generateURL(config){
+    let connectURL = `${baseURL}?services=${JSON.stringify(config.services)}&redirectUrl=${config.redirectUrl}&publicKey=${config.publicKey}`
+    return connectURL
+}
+
+let youtubeConfig = {
     publicKey: '0x026336a5b48e82e8764217f252b730cf1f90807055b7afd201e3ceb62d39813846',
     redirectUrl: 'https://blastscan.io/',
     services: {'youtube':true}
 }
 
-let connectURL = `${baseURL}?services=${JSON.stringify(config.services)}&redirectUrl=${config.redirectUrl}&publicKey=${config.publicKey}`
+let netflixConfig = {
+    publicKey: '0x026336a5b48e82e8764217f252b730cf1f90807055b7afd201e3ceb62d39813846',
+    redirectUrl: 'https://blastscan.io/',
+    services: {'netflix':true}
+}
+
+let combinedConfig = {
+    publicKey: '0x026336a5b48e82e8764217f252b730cf1f90807055b7afd201e3ceb62d39813846',
+    redirectUrl: 'https://blastscan.io/',
+    services: {'netflix':false,'youtube':true}
+}
 
 
-
-
-console.log("🔗",connectURL)
+console.log("🔗 Netflix  : ",generateURL(netflixConfig))
+console.log("🔗 YouTube  : ",generateURL(youtubeConfig))
+console.log("🔗 Combined : ",generateURL(combinedConfig))
